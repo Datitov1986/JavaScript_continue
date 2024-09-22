8e8c3ed7054c82b12439f9222de326892bfab9f9
Dmitri <datitov2009@yandex.ru>
2024-09-22 14:00:43 +0200

    add homework 5

---
+++ JS_homework5/data.js
@@ -0,0 +1,5 @@
+export const characters = await fetch('https://rickandmortyapi.com/api/character')
+    .then(response => response.json())
+    .catch(error => {
+        return { "error": error }
+    })
---
+++ JS_homework5/script.js
@@ -0,0 +1,29 @@
+import { characters } from './data.js'
+
+const content = document.querySelector('.content')
+
+if (!characters.error) {
+    characters.results.forEach(character => {
+        const characterBox = document.createElement('div')
+        characterBox.classList.add('character-box')
+
+        const characterImage = document.createElement('img')
+        characterImage.classList.add('image')
+        characterImage.src = character.image
+        characterImage.alt = character.name
+
+        const characterName = document.createElement('p')
+        characterName.classList.add('title')
+        characterName.textContent = character.name
+
+        const characterLocation = document.createElement('p')
+        characterLocation.textContent = `Родился в: ${character.location.name}`
+
+        characterBox.insertAdjacentElement('beforeend', characterImage)
+        characterBox.insertAdjacentElement('beforeend', characterName)
+        characterBox.insertAdjacentElement('beforeend', characterLocation)
+        content.insertAdjacentElement('beforeend', characterBox)
+    })
+} else {
+    alert(characters.error.message)
+}
